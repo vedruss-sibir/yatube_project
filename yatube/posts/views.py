46d0c8f7fdc6b9da9ff37b5b3e1b77095faf5e79
Андрей Стрельников <andre33_3@rambler.ru>
2021-12-19 18:42:05 +0600

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Здравствуйте, здесь будут посты {slug}')
